c0a120cbfae9692e7dae404da96ad031c4a775b7
Wouter Jansen <wouter97@planet.nl>
2019-02-07 20:33:52 +0100

    Finished lecture 4

--- src/Lectures/Basics/Functions.scala
+++ src/Lectures/Basics/Functions.scala
@@ -30,13 +30,33 @@ object Functions extends App {
   }
 
   //1. Greet a person (name, age) => Hi my name is $name, and I am $age years old
-  def greetAPerson(name: String, age: Int): Unit ={
-    println("Hi my name is: " + name + " And I am " + age + " years old.")
+  def greetAPerson(name: String, age: Int): String ={
+    "Hi my name is: " + name + " and I am " + age + " years old."
   }
   greetAPerson("Barry", 69)
   //2. Factorial function 1 * 2 * 3 * ... * n (recursive)
+  def factorial(n: Int): Int =
+    if(n <= 0) 1
+    else n * factorial(n-1)
+
+    println(factorial(5)) //120
 
   //3. Fibonacci function f(1) = 1 f(2) = 1 (fn) = f(n - 1) + f(n - 2)
+  def fibonacci(n: Int): Int =
+    if (n <= 2) 1
+    else fibonacci(n -1) + fibonacci(n - 2)
+
+  println(fibonacci(8))
 
   //4. Test if a number is prime
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean =
+      if(t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+
+    isPrimeUntil(n / 2)
+  }
+  println(isPrime(37))
+  println(isPrime(2003))
+  println(isPrime(37 * 17))
 }
